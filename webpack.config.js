9b4a7f81d2c2da66d3357bc28dfd758d36266890
timqian <timqian92@qq.com>
2016-02-25 15:06:17 +0800

    structure

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: './build',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
+  }
+};
